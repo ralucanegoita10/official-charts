3b6ef377f2bd2864ab89796598a71c6a888166cf
Raluca Negoita <raluca.negoita@infinityworks.com>
2023-11-10 11:25:28 +0000

    Added sales table to  product page

--- src/features/transactions/index.js
+++ src/features/transactions/index.js
@@ -1,128 +1,133 @@
-import moment from "moment"
-import { useEffect, useState } from "react"
-import { useDispatch, useSelector } from "react-redux"
-import { showNotification } from "../common/headerSlice"
-import TitleCard from "../../components/Cards/TitleCard"
-import { RECENT_TRANSACTIONS } from "../../utils/dummyData"
-import FunnelIcon from '@heroicons/react/24/outline/FunnelIcon'
-import XMarkIcon from '@heroicons/react/24/outline/XMarkIcon'
-import SearchBar from "../../components/Input/SearchBar"
+import moment from "moment";
+import { useEffect, useState } from "react";
 
-const TopSideButtons = ({removeFilter, applyFilter, applySearch}) => {
+import TitleCard from "../../components/Cards/TitleCard";
+import { RECENT_TRANSACTIONS } from "../../utils/dummyData";
+import { Title } from "chart.js";
+import SalesData from "./salesTabledata";
+import "./productDetails.css";
 
-    const [filterParam, setFilterParam] = useState("")
-    const [searchText, setSearchText] = useState("")
-    const locationFilters = ["Paris", "London", "Canada", "Peru", "Tokyo"]
+function Transactions() {
+  const [trans, setTrans] = useState(RECENT_TRANSACTIONS);
 
-    const showFiltersAndApply = (params) => {
-        applyFilter(params)
-        setFilterParam(params)
-    }
+  const generateLoadingBarStyle = (value) => {
+    const parsedValue = parseFloat(value.replace(/,/g, ""));
+    const percentage = (parsedValue / 5000000) * 100; // Assuming maximum value is 50,000
+    const barColor = percentage <= 60 ? "#0f27e9" : "#ef6ad4"; // Blue if filled less than half, pink if filled more than half
 
-    const removeAppliedFilter = () => {
-        removeFilter()
-        setFilterParam("")
-        setSearchText("")
-    }
+    return {
+      width: `${percentage}%`,
+      backgroundColor: barColor,
+    };
+  };
 
-    useEffect(() => {
-        if(searchText == ""){
-            removeAppliedFilter()
-        }else{
-            applySearch(searchText)
-        }
-    }, [searchText])
-
-    return(
-        <div className="inline-block float-right">
-            <SearchBar searchText={searchText} styleClass="mr-4" setSearchText={setSearchText}/>
-            {filterParam != "" && <button onClick={() => removeAppliedFilter()} className="btn btn-xs mr-2 btn-active btn-ghost normal-case">{filterParam}<XMarkIcon className="w-4 ml-2"/></button>}
-            <div className="dropdown dropdown-bottom dropdown-end">
-                <label tabIndex={0} className="btn btn-sm btn-outline"><FunnelIcon className="w-5 mr-2"/>Filter</label>
-                <ul tabIndex={0} className="dropdown-content menu p-2 text-sm shadow bg-base-100 rounded-box w-52">
-                    {
-                        locationFilters.map((l, k) => {
-                            return  <li key={k}><a onClick={() => showFiltersAndApply(l)}>{l}</a></li>
-                        })
-                    }
-                    <div className="divider mt-0 mb-0"></div>
-                    <li><a onClick={() => removeAppliedFilter()}>Remove Filter</a></li>
-                </ul>
-            </div>
+  return (
+    <>
+      <TitleCard title="Product details" topMargin="mt-2">
+        <div className="overflow-x-auto w-full">
+          <table className="table w-full">
+            <thead>
+              <tr>
+                <th> </th>
+                <th>Chart Eligible</th>
+                <th>Physical</th>
+                <th>Download</th>
+                <th>Aud Strm Prem</th>
+                <th>Aud Strm Prem Calc</th>
+                <th>Aud Strm AdF </th>
+                <th>Aud Strm AdF Calc</th>
+                <th>Vid Strm Prem</th>
+                <th>Vid Strm Prem Calc</th>
+                <th>Vid Strm AdF</th>
+                <th>Vid Strm AdF Calc</th>
+              </tr>
+            </thead>
+            <tbody>
+              {SalesData.map((data, index) => {
+                return (
+                  <tr key={index}>
+                    <td>{data.Type}</td>
+                    
+                    <td>{data.chartEligible}</td>
+                    <td>{data.Physical}</td>
+                    <td>
+                      <div className="loading-bar-container">
+                        <div
+                          className="loading-bar"
+                          style={generateLoadingBarStyle(data.Download)}
+                        ></div>
+                      </div>
+                      <div className="loading-bar-label">{data.Download}</div>
+                    </td>
+                    <td>
+                      <div className="loading-bar-container">
+                        <div
+                          className="loading-bar"
+                          style={generateLoadingBarStyle(data.AudStrmPrem)}
+                        ></div>
+                      </div>
+                      <div className="loading-bar-label">{data.AudStrmPrem}</div>
+                    </td>
+                    <td>
+                      <div className="loading-bar-container">
+                        <div
+                          className="loading-bar"
+                          style={generateLoadingBarStyle(data.AudStrmPremCalc)}
+                        ></div>
+                      </div>
+                      <div className="loading-bar-label">{data.AudStrmPremCalc}</div>
+                    </td>
+                    <td>
+                      <div className="loading-bar-container">
+                        <div
+                          className="loading-bar"
+                          style={generateLoadingBarStyle(data.AudStrmAdF)}
+                        ></div>
+                      </div>
+                      <div className="loading-bar-label">{data.AudStrmAdF}</div>
+                    </td>
+                    <td>
+                      <div className="loading-bar-container">
+                        <div
+                          className="loading-bar"
+                          style={generateLoadingBarStyle(data.AudStrmAdFCalc)}
+                        ></div>
+                      </div>
+                      <div className="loading-bar-label">{data.AudStrmAdFCalc}</div>
+                    </td>
+                    <td>
+                      <div className="loading-bar-container">
+                        <div
+                          className="loading-bar"
+                          style={generateLoadingBarStyle(data.VidStrmPrem)}
+                        ></div>
+                      </div>
+                      <div className="loading-bar-label">{data.VidStrmPrem}</div>
+                    </td>
+                    <td>
+                      <div className="loading-bar-container">
+                        <div
+                          className="loading-bar"
+                          style={generateLoadingBarStyle(data.VidStrmPremCalc)}
+                        ></div>
+                      </div>
+                      <div className="loading-bar-label">{data.VidStrmPremCalc}</div>
+                    </td>
+                    <td>{data.VidStrmAdF}</td>
+                    <td>{data.VidStrmAdFCalc}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
         </div>
-    )
+      </TitleCard>
+
+      <TitleCard title="Sales"></TitleCard>
+
+      <TitleCard title="Sales history" topMargin="mt-2"></TitleCard>
+    </>
+  );
 }
 
-
-function Transactions(){
-
-
-    const [trans, setTrans] = useState(RECENT_TRANSACTIONS)
-
-    const removeFilter = () => {
-        setTrans(RECENT_TRANSACTIONS)
-    }
-
-    const applyFilter = (params) => {
-        let filteredTransactions = RECENT_TRANSACTIONS.filter((t) => {return t.location == params})
-        setTrans(filteredTransactions)
-    }
-
-    // Search according to name
-    const applySearch = (value) => {
-        let filteredTransactions = RECENT_TRANSACTIONS.filter((t) => {return t.email.toLowerCase().includes(value.toLowerCase()) ||  t.email.toLowerCase().includes(value.toLowerCase())})
-        setTrans(filteredTransactions)
-    }
-
-    return(
-        <>
-            
-            <TitleCard title="Recent Transactions" topMargin="mt-2" TopSideButtons={<TopSideButtons applySearch={applySearch} applyFilter={applyFilter} removeFilter={removeFilter}/>}>
-
-                {/* Team Member list in table format loaded constant */}
-            <div className="overflow-x-auto w-full">
-                <table className="table w-full">
-                    <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Email Id</th>
-                        <th>Location</th>
-                        <th>Amount</th>
-                        <th>Transaction Date</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            trans.map((l, k) => {
-                                return(
-                                    <tr key={k}>
-                                    <td>
-                                        <div className="flex items-center space-x-3">
-                                            <div className="avatar">
-                                                <div className="mask mask-circle w-12 h-12">
-                                                    <img src={l.avatar} alt="Avatar" />
-                                                </div>
-                                            </div>
-                                            <div>
-                                                <div className="font-bold">{l.name}</div>
-                                            </div>
-                                        </div>
-                                    </td>
-                                    <td>{l.email}</td>
-                                    <td>{l.location}</td>
-                                    <td>${l.amount}</td>
-                                    <td>{moment(l.date).format("D MMM")}</td>
-                                    </tr>
-                                )
-                            })
-                        }
-                    </tbody>
-                </table>
-            </div>
-            </TitleCard>
-        </>
-    )
-}
-
-
-export default Transactions
+export default Transactions;
