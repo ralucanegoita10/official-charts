9b7c5fd6d42f939a053aebd1c2b7d5475f9e7709
Raluca Negoita <raluca.negoita@infinityworks.com>
2023-11-10 15:23:32 +0000

    Styled  sales history table product page

--- src/features/transactions/index.js
+++ src/features/transactions/index.js
@@ -21,254 +21,275 @@ function Transactions() {
 
   const styles = {
     key: {
-      color: "#ef6ad4",
+      color: "black",
       fontWeight: "bold",
-      backgroundColor: "black"
-
+      backgroundColor: "rgb(242, 242, 242)",
     },
     value: {
-      color: "white",
-      backgroundColor: "black",
-
+      color: "black",
+      backgroundColor: "rgb(242, 242, 242)",
     },
     table: {
-        width: "400px",
-    }
+      width: "400px",
+    },
   };
 
   return (
     <>
-      <TitleCard title="Sales" topMargin="mt-2">
-        <div className="overflow-x-auto w-full">
-          <table className="table w-full">
-            <thead>
-              <tr>
-                <th> </th>
-                <th>Chart Eligible</th>
-                <th>Physical</th>
-                <th>Download</th>
-                <th>Aud Strm Prem</th>
-                <th>Aud Strm Prem Calc</th>
-                <th>Aud Strm AdF </th>
-                <th>Aud Strm AdF Calc</th>
-                <th>Vid Strm Prem</th>
-                <th>Vid Strm Prem Calc</th>
-                <th>Vid Strm AdF</th>
-                <th>Vid Strm AdF Calc</th>
-              </tr>
-            </thead>
-            <tbody>
-              {SalesData.map((data, index) => {
-                return (
+      <div
+        style={{
+          display: "flex",
+          gap: "2rem",
+          maxWidth: "100vw",
+          overflowX: "auto",
+        }}
+      >
+        <TitleCard
+          title="Product Details"
+          topMargin="mt-2"
+          style={{ flex: "0 0 30%" }}
+        >
+          <div style={styles.table} className="w-1/2">
+            <table className="table w-2/3">
+              <tbody>
+                {Object.entries(ProductData[0]).map(([key, value], index) => (
                   <tr key={index}>
-                    <td>{data.Type}</td>
-
-                    <td>{data.chartEligible}</td>
-                    <td>{data.Physical}</td>
-                    <td>
-                      <div className="loading-bar-container">
-                        <div
-                          className="loading-bar"
-                          style={generateLoadingBarStyle(data.Download)}
-                        ></div>
-                      </div>
-                      <div className="loading-bar-label">{data.Download}</div>
-                    </td>
-                    <td>
-                      <div className="loading-bar-container">
-                        <div
-                          className="loading-bar"
-                          style={generateLoadingBarStyle(data.AudStrmPrem)}
-                        ></div>
-                      </div>
-                      <div className="loading-bar-label">
-                        {data.AudStrmPrem}
-                      </div>
-                    </td>
-                    <td>
-                      <div className="loading-bar-container">
-                        <div
-                          className="loading-bar"
-                          style={generateLoadingBarStyle(data.AudStrmPremCalc)}
-                        ></div>
-                      </div>
-                      <div className="loading-bar-label">
-                        {data.AudStrmPremCalc}
-                      </div>
-                    </td>
-                    <td>
-                      <div className="loading-bar-container">
-                        <div
-                          className="loading-bar"
-                          style={generateLoadingBarStyle(data.AudStrmAdF)}
-                        ></div>
-                      </div>
-                      <div className="loading-bar-label">{data.AudStrmAdF}</div>
-                    </td>
-                    <td>
-                      <div className="loading-bar-container">
-                        <div
-                          className="loading-bar"
-                          style={generateLoadingBarStyle(data.AudStrmAdFCalc)}
-                        ></div>
-                      </div>
-                      <div className="loading-bar-label">
-                        {data.AudStrmAdFCalc}
-                      </div>
-                    </td>
-                    <td>
-                      <div className="loading-bar-container">
-                        <div
-                          className="loading-bar"
-                          style={generateLoadingBarStyle(data.VidStrmPrem)}
-                        ></div>
-                      </div>
-                      <div className="loading-bar-label">
-                        {data.VidStrmPrem}
-                      </div>
-                    </td>
-                    <td>
-                      <div className="loading-bar-container">
-                        <div
-                          className="loading-bar"
-                          style={generateLoadingBarStyle(data.VidStrmPremCalc)}
-                        ></div>
-                      </div>
-                      <div className="loading-bar-label">
-                        {data.VidStrmPremCalc}
-                      </div>
-                    </td>
-                    <td>{data.VidStrmAdF}</td>
-                    <td>{data.VidStrmAdFCalc}</td>
+                    <td style={styles.key}>{key}</td>
+                    <td style={styles.value}>{value}</td>
                   </tr>
-                );
-              })}
-            </tbody>
-          </table>
-        </div>
-      </TitleCard>
-
-      <TitleCard title="Product Details" topMargin="mt-2">
-        <div style={styles.table} className="overflow-x-auto w-full">
-          <table className="table w-full" >
-            <tbody>
-              {Object.entries(ProductData[0]).map(([key, value], index) => (
-                <tr key={index}>
-                  <td style={styles.key}>{key}</td>
-                  <td style={styles.value}>{value}</td>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </TitleCard>
+        <TitleCard
+          title="Sales History Details"
+          topMargin="mt-2"
+          style={{ flex: 1 }}
+        >
+          <div className="overflow-x-auto w-full">
+            <table className="table w-full">
+              <thead>
+                <tr>
+                  <th rowSpan="2"></th>
+                  <th colSpan="3">Market Contribution DUS</th>
+                  <th colSpan="3">Product Contribution DUS</th>
+                  <th rowSpan="2">Score DUS</th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-      </TitleCard>
+                <tr>
+                  <th>2342</th>
+                  <th>2341</th>
+                  <th>% Var</th>
+                  <th>2342</th>
+                  <th>2341</th>
+                  <th>% Var</th>
+                </tr>
+              </thead>
+              <tbody>
+                {SalesHistoryTabledata.map((data, index) => (
+                  <React.Fragment key={index}>
+                    <tr>
+                      <td rowSpan="2">{data.ShowPercentages}</td>
+                    </tr>
+                    <tr>
+                      <td>
+                        <div className="loading-bar-container">
+                          <div
+                            className="loading-bar"
+                            style={generateLoadingBarStyle(
+                              data.MarketContributionDUS.data[0]
+                            )}
+                          ></div>
+                        </div>
+                        <div className="loading-bar-label">
+                          {data.MarketContributionDUS.data[0]}
+                        </div>
+                      </td>
+                      <td>
+                        <div className="loading-bar-container">
+                          <div
+                            className="loading-bar"
+                            style={generateLoadingBarStyle(
+                              data.MarketContributionDUS.data[1]
+                            )}
+                          ></div>
+                        </div>
+                        <div className="loading-bar-label">
+                          {data.MarketContributionDUS.data[1]}
+                        </div>
+                      </td>
+                      <td>
+                        <div className="loading-bar-container">
+                          <div
+                            className="loading-bar"
+                            style={generateLoadingBarStyle(
+                              data.MarketContributionDUS.data[2]
+                            )}
+                          ></div>
+                        </div>
+                        <div className="loading-bar-label">
+                          {data.MarketContributionDUS.data[2]}
+                        </div>
+                      </td>
 
-      <TitleCard title="Sales History Details" topMargin="mt-2">
-        <div className="overflow-x-auto w-full">
-          <table className="table w-full">
-            <thead>
-              <tr>
-                <th rowSpan="2">Show Percentages</th>
-                <th colSpan="3">Market Contribution DUS</th>
-                <th colSpan="3">Product Contribution DUS</th>
-                <th rowSpan="2">Score DUS</th>
-              </tr>
-              <tr>
-                <th>2342</th>
-                <th>2341</th>
-                <th>% Var</th>
-                <th>2342</th>
-                <th>2341</th>
-                <th>% Var</th>
-              </tr>
-            </thead>
-            <tbody>
-              {SalesHistoryTabledata.map((data, index) => (
-                <React.Fragment key={index}>
-                  <tr>
-                    <td rowSpan="2">{data.ShowPercentages}</td>
-                  </tr>
-                  <tr>
-                    <td>
-                      <div className="loading-bar-container">
-                        <div
-                          className="loading-bar"
-                          style={generateLoadingBarStyle(
-                            data.MarketContributionDUS.data[0]
-                          )}
-                        ></div>
-                      </div>
-                      <div className="loading-bar-label">
-                        {data.MarketContributionDUS.data[0]}
-                      </div>
-                    </td>
-                    <td>
-                      <div className="loading-bar-container">
-                        <div
-                          className="loading-bar"
-                          style={generateLoadingBarStyle(
-                            data.MarketContributionDUS.data[1]
-                          )}
-                        ></div>
-                      </div>
-                      <div className="loading-bar-label">
-                        {data.MarketContributionDUS.data[1]}
-                      </div>
-                    </td>
-                    <td>
-                      <div className="loading-bar-container">
-                        <div
-                          className="loading-bar"
-                          style={generateLoadingBarStyle(
-                            data.MarketContributionDUS.data[2]
-                          )}
-                        ></div>
-                      </div>
-                      <div className="loading-bar-label">
-                        {data.MarketContributionDUS.data[2]}
-                      </div>
-                    </td>
-
-                    {/* <td>{data.ProductContributionDUS.data[0]}</td>
+                      {/* <td>{data.ProductContributionDUS.data[0]}</td>
                     <td>{data.ProductContributionDUS.data[1]}</td>
                     <td>{data.ProductContributionDUS.data[2]}</td> */}
-                    <td>
-                      <div className="loading-bar-container">
-                        <div
-                          className="loading-bar"
-                          style={generateLoadingBarStyle(
-                            data.ProductContributionDUS.data[0]
-                          )}
-                        ></div>
-                      </div>
-                      <div className="loading-bar-label">
-                        {data.ProductContributionDUS.data[0]}
-                      </div>
-                    </td>
-                    <td>
-                      <div className="loading-bar-container">
-                        <div
-                          className="loading-bar"
-                          style={generateLoadingBarStyle(
-                            data.ProductContributionDUS.data[1]
-                          )}
-                        ></div>
-                      </div>
-                      <div className="loading-bar-label">
-                        {data.ProductContributionDUS.data[1]}
-                      </div>
-                    </td>
-                    <td>{data.ProductContributionDUS.data[2]}</td>
+                      <td>
+                        <div className="loading-bar-container">
+                          <div
+                            className="loading-bar"
+                            style={generateLoadingBarStyle(
+                              data.ProductContributionDUS.data[0]
+                            )}
+                          ></div>
+                        </div>
+                        <div className="loading-bar-label">
+                          {data.ProductContributionDUS.data[0]}
+                        </div>
+                      </td>
+                      <td>
+                        <div className="loading-bar-container">
+                          <div
+                            className="loading-bar"
+                            style={generateLoadingBarStyle(
+                              data.ProductContributionDUS.data[1]
+                            )}
+                          ></div>
+                        </div>
+                        <div className="loading-bar-label">
+                          {data.ProductContributionDUS.data[1]}
+                        </div>
+                      </td>
+                      <td>{data.ProductContributionDUS.data[2]}</td>
 
-                    <td>{data.ScoreDUS}</td>
-                    <td></td>
-                  </tr>
-                </React.Fragment>
-              ))}
-            </tbody>
-          </table>
-        </div>
-      </TitleCard>
+                      <td>{data.ScoreDUS}</td>
+                      <td></td>
+                    </tr>
+                  </React.Fragment>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </TitleCard>
+      </div>
+      <div>
+        <TitleCard title="Sales" topMargin="mt-2" style={{ width: "60%" }}>
+          <div className="overflow-x-auto w-full">
+            <table className="table w-full">
+              <thead>
+                <tr>
+                  <th> </th>
+                  <th>Chart Eligible</th>
+                  <th>Physical</th>
+                  <th>Download</th>
+                  <th>Aud Strm Prem</th>
+                  <th>Aud Strm Prem Calc</th>
+                  <th>Aud Strm AdF </th>
+                  <th>Aud Strm AdF Calc</th>
+                  <th>Vid Strm Prem</th>
+                  <th>Vid Strm Prem Calc</th>
+                  <th>Vid Strm AdF</th>
+                  <th>Vid Strm AdF Calc</th>
+                </tr>
+              </thead>
+              <tbody>
+                {SalesData.map((data, index) => {
+                  return (
+                    <tr key={index}>
+                      <td>{data.Type}</td>
+
+                      <td>{data.chartEligible}</td>
+                      <td>{data.Physical}</td>
+                      <td>
+                        <div className="loading-bar-container">
+                          <div
+                            className="loading-bar"
+                            style={generateLoadingBarStyle(data.Download)}
+                          ></div>
+                        </div>
+                        <div className="loading-bar-label">{data.Download}</div>
+                      </td>
+                      <td>
+                        <div className="loading-bar-container">
+                          <div
+                            className="loading-bar"
+                            style={generateLoadingBarStyle(data.AudStrmPrem)}
+                          ></div>
+                        </div>
+                        <div className="loading-bar-label">
+                          {data.AudStrmPrem}
+                        </div>
+                      </td>
+                      <td>
+                        <div className="loading-bar-container">
+                          <div
+                            className="loading-bar"
+                            style={generateLoadingBarStyle(
+                              data.AudStrmPremCalc
+                            )}
+                          ></div>
+                        </div>
+                        <div className="loading-bar-label">
+                          {data.AudStrmPremCalc}
+                        </div>
+                      </td>
+                      <td>
+                        <div className="loading-bar-container">
+                          <div
+                            className="loading-bar"
+                            style={generateLoadingBarStyle(data.AudStrmAdF)}
+                          ></div>
+                        </div>
+                        <div className="loading-bar-label">
+                          {data.AudStrmAdF}
+                        </div>
+                      </td>
+                      <td>
+                        <div className="loading-bar-container">
+                          <div
+                            className="loading-bar"
+                            style={generateLoadingBarStyle(data.AudStrmAdFCalc)}
+                          ></div>
+                        </div>
+                        <div className="loading-bar-label">
+                          {data.AudStrmAdFCalc}
+                        </div>
+                      </td>
+                      <td>
+                        <div className="loading-bar-container">
+                          <div
+                            className="loading-bar"
+                            style={generateLoadingBarStyle(data.VidStrmPrem)}
+                          ></div>
+                        </div>
+                        <div className="loading-bar-label">
+                          {data.VidStrmPrem}
+                        </div>
+                      </td>
+                      <td>
+                        <div className="loading-bar-container">
+                          <div
+                            className="loading-bar"
+                            style={generateLoadingBarStyle(
+                              data.VidStrmPremCalc
+                            )}
+                          ></div>
+                        </div>
+                        <div className="loading-bar-label">
+                          {data.VidStrmPremCalc}
+                        </div>
+                      </td>
+                      <td>{data.VidStrmAdF}</td>
+                      <td>{data.VidStrmAdFCalc}</td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
+        </TitleCard>
+      </div>
     </>
   );
 }
--- src/features/transactions/salesHistory.js
+++ src/features/transactions/salesHistory.js
@@ -2,11 +2,11 @@ const SalesHistoryTabledata = [
   {
     ShowPercentages: "TOTALS",
     MarketContributionDUS: {
-      data: ["27,987,654", "35,123,456", "-1.2%"],
+      data: ["1,987,654", "2,123,456", "-1.2%"],
     },
     ProductContributionDUS: {
       headers: ["2348", "2347", "%Var"],
-      data: ["2,987,654", "5,123,456", "N/A"],
+      data: ["2,987,654", "1,123,456", "N/A"],
     },
     ScoreDUS: "N/A",
   },
@@ -16,37 +16,37 @@ const SalesHistoryTabledata = [
       data: ["2,543,210", "3,678,901", "-0.7%"],
     },
     ProductContributionDUS: {
-      data: ["6,543,210", "4,678,901", "N/A"],
+      data: ["2,543,210", "4,678,901", "N/A"],
     },
     ScoreDUS: "N/A",
   },
   {
     ShowPercentages: "Digital Bundle",
     MarketContributionDUS: {
-      data: ["27,654,321", "35,789,012", "-1.5%"],
+      data: ["2,654,321", "2,789,012", "-1.5%"],
     },
     ProductContributionDUS: {
-      data: ["27,654,321", "3,789,012", "N/A"],
+      data: ["2,654,321", "3,789,012", "N/A"],
     },
     ScoreDUS: "N/A",
   },
   {
     ShowPercentages: "Digital Track",
     MarketContributionDUS: {
-      data: ["26,876,543", "34,012,345", "-0.9%"],
+      data: ["1,876,543", "4,012,345", "-0.9%"],
     },
     ProductContributionDUS: {
-      data: ["26,876,543", "3,012,345", "N/A"],
+      data: ["2,876,543", "3,012,345", "N/A"],
     },
     ScoreDUS: "N/A",
   },
   {
     ShowPercentages: "REGIONS",
     MarketContributionDUS: {
-      data: ["2,123,456", "6,987,654", "-1.8%"],
+      data: ["2,123,456", "2,987,654", "-1.8%"],
     },
     ProductContributionDUS: {
-      data: ["8,123,456", "6,987,654", "N/A"],
+      data: ["1,123,456", "1,987,654", "N/A"],
     },
     ScoreDUS: "N/A",
   },
@@ -56,34 +56,34 @@ const SalesHistoryTabledata = [
       data: ["2,234,567", "3,345,678", "-0.6%"],
     },
     ProductContributionDUS: {
-      data: ["2,234,567", "34,345,678", "N/A"],
+      data: ["2,234,567", "4,345,678", "N/A"],
     },
     ScoreDUS: "N/A",
   },
   {
     ShowPercentages: "South",
     MarketContributionDUS: {
-      data: ["7,567,890", "5,987,654", "-1.3%"],
+      data: ["1,567,890", "2,987,654", "-1.3%"],
     },
     ProductContributionDUS: {
-      data: ["7,567,890", "1,987,654", "N/A"],
+      data: ["2,567,890", "1,987,654", "N/A"],
     },
     ScoreDUS: "N/A",
   },
   {
     ShowPercentages: "South West",
     MarketContributionDUS: {
-      data: ["6,789,012", "4,210,987", "-0.8%"],
+      data: ["4,789,012", "4,210,987", "-0.8%"],
     },
     ProductContributionDUS: {
-      data: ["2,789,012", "34,210,987", "N/A"],
+      data: ["2,789,012", "4,210,987", "N/A"],
     },
     ScoreDUS: "N/A",
   },
   {
     ShowPercentages: "Wales & West",
     MarketContributionDUS: {
-      data: ["8,345,678", "3,123,456", "-2.1%"],
+      data: ["2,345,678", "3,123,456", "-2.1%"],
     },
     ProductContributionDUS: {
       data: ["2,345,678", "3,123,456", "N/A"],
@@ -93,7 +93,7 @@ const SalesHistoryTabledata = [
   {
     ShowPercentages: "Midlands",
     MarketContributionDUS: {
-      data: ["7,234,567", "6,345,678", "-1.7%"],
+      data: ["3,234,567", "2,345,678", "-1.7%"],
     },
     ProductContributionDUS: {
       data: ["2,234,567", "3,345,678", "N/A"],
